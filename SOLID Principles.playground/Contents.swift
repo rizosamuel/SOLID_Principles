4881e846df8c9ee12c54ffaaafeabbd6a484f520
Rijo <rizosamuel1995@gmail.com>
2023-09-13 12:39:33 +0530

    Added SOLID acronym explanation

--- SOLID Principles.playground/Contents.swift
+++ SOLID Principles.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/* SOLID is an acronym that represents a set of five design principles for writing maintainable and scalable software. These principles were introduced by Robert C. Martin and have become fundamental concepts in object-oriented programming and software design. */
+
+// S - Single Responsibility Principle
+// O - Open/Closed Principle
+// L - Liskov Substitution Principle
+// I - Interface Segregation Principle
+// D - Dependency Inversion Principle
